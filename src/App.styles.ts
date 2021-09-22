def6bf2a963ed509d3979959c570fe28c4dc71f3
Maya Liberman <mayaliberman1@gmail.com>
2021-09-22 14:02:39 -0400

    first commit

---
+++ src/App.styles.ts
@@ -0,0 +1,69 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import BGImage from './images/syd-sujuaan.jpg';
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  height: 100%;
+}
+
+body {
+  background-image: url(${BGImage});
+  background-size: cover;
+  margin: 0;
+  padding: 0 20px;
+  display: flex;
+  justify-content: center;
+}
+
+* {
+  box-sizing: border-box;
+  font-family: 'Catamaran', sans-serif;
+}
+`
+
+export const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+> p {
+  color: white;
+}
+
+.score {
+  color: white;
+  font-size: 2rem;
+  margin: 0;
+}
+
+h1 {
+  font-family: Fascinate Inline, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+   background-image: linear-gradient(45deg, #f3ec78, #af4261);
+  background-size: 100%;
+  background-clip: text;
+ text-fill-color: transparent;
+ -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent; 
+    -moz-text-fill-color: transparent;
+ 
+  font-size: 70px;
+  text-align: center;
+  font-weight: 400;
+  margin: 20px;
+}
+
+.start, .next {
+  cursor: pointer;
+  background: linear-gradient(180deg, white, #ffcc91);
+  border: 2px solid #d38558;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  height: 40px;
+  margin: 20px 0;
+  padding: 0 40px;
+}
+
+.start {
+  max-width: 200px;
+}
+`
